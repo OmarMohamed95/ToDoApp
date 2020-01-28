efcd6bed0f6140884743ce79a1b30ac4741086e0
omar mohamed <omar.mo9516@gmail.com>
2020-01-28 10:08:12 +0200

    Move functions to the store

--- assets/js/components/taskAdd.vue
+++ assets/js/components/taskAdd.vue
@@ -1,17 +1,15 @@
 <template>
 <div>
     <h1 class="mt-5">Add Task</h1>
-    <span class="success_message" v-show="res.success">{{ res.success }}</span>
-    <div v-if="loading">
-        <p class="text-center"><font-awesome-icon class="fa-pulse fa-3x" :icon="['fas', 'spinner']" /></p>
-    </div>
-    <form v-else action="http://127.0.0.1:8000/api/task" method="post" ref="taskForm" id="taskForm" v-on:submit.prevent="addTask()">
+    <span class="success_message" v-show="message.success">{{ message.success }}</span>
+    <loading v-if="loading"></loading>
+      <form v-else action="http://127.0.0.1:8000/api/task" method="post" ref="taskForm" id="taskForm" v-on:submit.prevent="addTask()">
         <div class="mt-5 col-md-10 task-add-con">
             <div class="form-group row mt-2">
                 <label for="title" class="label col-md-2">title</label>
                 <div class="col-md-10">
                     <input type="text" name="title" class="form-control" id="title" ref="task_title" placeholder="title">
-                    <p class="validation-error" v-show="res.title">{{ res.title }}</p>
+                    <p class="validation-error" v-show="message.title">{{ message.title }}</p>
                 </div>
             </div>
             <div class="form-group row mt-2">
@@ -26,7 +24,7 @@
                     <select name="list" class="form-control" id="note" ref="task_note">
                         <option :key="k" v-for="(i, k) in lists" :value="i.id">{{ i.title }}</option>
                     </select>
-                    <p class="validation-error" v-show="res.list">{{ res.list }}</p>
+                    <p class="validation-error" v-show="message.list">{{ message.list }}</p>
                 </div>
             </div>
             <div class="form-group row mt-2 task-add-priority-con">
@@ -48,14 +46,14 @@
                         <input type="radio" value="4" name="priority" class="form-control" id="priority" ref="task_priority">
                         <label class="form-check-label" for="exampleRadios1">very high</label>
                     </div>
-                    <p class="validation-error" v-show="res.priority">{{ res.priority }}</p>
+                    <p class="validation-error" v-show="message.priority">{{ message.priority }}</p>
                 </div>
             </div>
             <div class="form-group row mt-2">
                 <label for="run_at" class="label col-md-2">run at</label>
                 <div class="col-md-10">
                     <datetime type="datetime" name="run_at" id="run_at" ref="task_run_at" input-class="form-control" v-model="datetime12" use12-hour></datetime>
-                    <p class="validation-error" v-show="res.run_at">{{ res.run_at }}</p>
+                    <p class="validation-error" v-show="message.run_at">{{ message.run_at }}</p>
                 </div>
             </div>
             <div class="form-group mt-2">
@@ -68,61 +66,52 @@
 
 <script type="text/javascript">
 
-import { Datetime } from 'vue-datetime';
+import loading from './loading';
 import axios from 'axios'
+import { Datetime } from 'vue-datetime';
 
 export default {
     name: 'taskAdd',
     props:['uploads_base_url'],
+    components: {
+        datetime: Datetime,
+        loading: loading,
+    },
     data: function(){
         return{
-            res: {},
-            lists: {},
-            loading: true,
+
         }
     },
-    components: {
-        datetime: Datetime
+    computed: {
+        loading(){
+            return this.$store.getters.getLoading
+        },
+        res(){
+            return this.$store.getters.getRes
+        },
+        message(){
+            return this.$store.getters.message
+        },
+        lists(){
+            return this.$store.getters.getLists
+        },
     },
     methods: {
+        getAllLists: function(){
+            this.$store.dispatch('getAllLists');
+        },
         addTask: function(){
             const form = document.getElementById('taskForm');
 
             let formData = new FormData(form);
 
-            axios({
-                url: this.$refs.taskForm.action,
-                method: 'POST', 
-                data: formData,
-            })
-            .then(res => {
-                if(res.status === 200)
-                {
-                    this.res = res.data
-                    this.$store.dispatch("pushNotification");
-                }
-            })
-            .catch(error => {
-                if(error.response.status === 400)
-                {
-                    this.res = error.response.data;
-                }
-                console.log(error);
-            })
+            this.$store.dispatch('addTask', formData);
 
         },
-        getAllLists: function(){
-            
-            axios.get('http://127.0.0.1:8000/api/list')
-            .then(res => {
-                this.loading = false;
-                this.lists = res.data;
-            })
-            .catch(console.error)
-        }
     },
     created() {
-        this.getAllLists()  
+        this.getAllLists();
+        this.$store.dispatch('clearMessage');
     },
 }
 </script>
