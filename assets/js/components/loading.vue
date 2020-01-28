ff0b9b709f1fe2b35c5bd97a9a52ee9a9452ca99
omar mohamed <omar.mo9516@gmail.com>
2020-01-28 09:59:28 +0200

    Create loading component

---
+++ assets/js/components/loading.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="loading-spinner">
+        <p class="text-center"><font-awesome-icon class="fa-pulse fa-3x" :icon="['fas', 'spinner']" /></p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.loading-spinner{
+    margin-top: 15%;
+}
+
+</style>
