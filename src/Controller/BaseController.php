fc51ffb720899d13a97b51d12d9fa26fcb3e85b7
omar mohamed <omar.mo9516@gmail.com>
2020-02-22 14:30:19 +0200

    create BaseController

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\Controller\Annotations\RequestParam;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Controller\Annotations\FileParam;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+abstract class BaseController extends AbstractFOSRestController
+{
+    public function baseView($data = null, $statusCode = 200, array $headers = [])
+    {
+        $view = $this->view($data, $statusCode, $headers);
+        return $this->handleView($view);
+    }
+}
