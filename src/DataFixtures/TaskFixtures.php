eaf50cc3abb0e34e4509a654a4abcb943b0aa9d4
OmarMohamed95 <omar.mo9516@gmail.com>
2020-07-11 22:38:59 +0200

    Change task fixtures referance name

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -13,17 +13,17 @@ class TaskFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         for($i=0; $i < 10; $i++) {
-            $list = new Tasks();
-            $list->setTitle('task' . $i);
-            $list->setNote('note' . $i);
-            $list->setPriority(1);
-            $list->setList($this->getReference(ListFixtures::getReferenceKey($i % ListFixtures::LIST_COUNT)));
-            $list->setIsDone(0);
-            $list->setIsNotified(0);
-            $list->setRunAt(date("Y-m-d", strtotime('tomorrow')));
+            $task = new Tasks();
+            $task->setTitle('task' . $i);
+            $task->setNote('note' . $i);
+            $task->setPriority(1);
+            $task->setList($this->getReference(ListFixtures::getReferenceKey($i % ListFixtures::LIST_COUNT)));
+            $task->setIsDone(0);
+            $task->setIsNotified(0);
+            $task->setRunAt(date("Y-m-d", strtotime('tomorrow')));
 
-            $manager->persist($list);
-            $this->addReference('task-' . $i, $list);
+            $manager->persist($task);
+            $this->addReference('task-' . $i, $task);
         }
 
         $manager->flush();
