2897ae4762994e5b044750d23cbe32196d340eb0
OmarMohamed95 <omar.mo9516@gmail.com>
2020-08-01 17:13:32 +0200

    Update BaseTest

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,27 +2,48 @@
 
 namespace App\Tests;
 
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class BaseTest extends WebTestCase
 {
+    use FixturesTrait;
+
     private $client;
 
-    protected function createAuthenticatedClient($email = 'test1@test.com', $password = 'test1')
+    /**
+     * Get service container
+     * 
+     * @return Container
+     */
+    protected function getContainer()
     {
+        self::bootKernel();
+        return self::$kernel->getContainer();
+    }
+
+    protected function getAuthenticatedClient()
+    {
+        $route = $this->getRoute('api_login_check');
+
         $this->client = static::createClient();
         $this->client->request(
         'POST',
-        'http://127.0.0.1:8000/api/login_check',
-        array(),
-        array(),
-        array('CONTENT_TYPE' => 'application/json'),
-        json_encode(array(
-            'email' => $email,
-            'password' => $password,
-            ))
+        $route,
+        [],
+        [],
+        ['CONTENT_TYPE' => 'application/json'],
+        json_encode([
+            'email' => 'test1@test.com',
+            'password' => 'test1',
+            ])
         );
 
         return $this->client;
+    }
+
+    protected function getRoute(string $routeName, array $parameters = [])
+    {
+        return $this->getContainer()->get('router')->generate($routeName, $parameters);
     }
 }
