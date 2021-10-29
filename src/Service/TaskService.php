74742f0080801c379d2b0d40a8bce1e363800468
OmarMohamed95 <omar.mo9516@gmail.com>
2021-10-29 14:49:20 +0200

    Create task service

---
+++ src/Service/TaskService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\TasksRepository;
+use App\Service\FileUploader;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Service\ValidationService;
+
+/**
+ * UserService
+ */
+class TaskService extends AbstractController
+{
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    /** @var TasksRepository */
+    private $taskRepository;
+
+    /** @var FileUploader */
+    private $fileUploader;
+
+    public function __construct(
+        FileUploader $fileUploader,
+        EntityManagerInterface $entityManager,
+        TasksRepository $taskRepository
+    ) {
+        $this->fileUploader = $fileUploader;
+        $this->entityManager = $entityManager;
+        $this->taskRepository = $taskRepository;
+    }
+
+    /**
+     * Get all task of a user
+     *
+     * @param User $user
+     *
+     * @return mixed
+     */
+    public function getTasksByUser(User $user)
+    {
+        return $this->taskRepository->getTasksByUser($user->getId());
+    }
+}
